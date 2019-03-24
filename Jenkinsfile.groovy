f2201f8a5b124a83a9c5ae1458a7fef2dde40227
AKMAL KADYROV <AKMALKADYROV@AKMALs-MacBook-Pro.local>
2019-03-24 15:10:19 -0600

    version added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,13 +1,12 @@
 node{
-    properties([parameters([run(description: 'Please enter IP address to run website', filter: 'ALL', name: '127.0.0.1', projectName: 'IP ')])])
-    stage("Run container"){
+properties([parameters([string(defaultValue: 'IP ', description: 'Please enter IP address for website', name: '127.0.0.1', trim: false), string(defaultValue: 'version ', description: 'Please select version ', name: 'VER', trim: false)])])    stage("Run container"){
         try{
              sh "ssh root@${IP} docker rm -f Flaskex"
          }
          catch(exec){
              sh "echo container deleted"
          }   
-        sh "ssh docker run -d Flaskex -p 4400:4000 farrukhsadyrkov/flaskex"
+        sh "ssh docker run -d Flaskex -p 4400:4000 farrukhsadyrkov/flaskex:${VER}"
     
     }
 }
